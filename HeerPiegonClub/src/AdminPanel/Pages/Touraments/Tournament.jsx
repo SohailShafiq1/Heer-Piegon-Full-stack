91f4cb1ff62da0fb5edf6efaa98048a30a95dd10
SohailShafiq1 <sohail.shafiq002@gmail.com>
2025-03-19 00:51:29 +0500

    Tournaments Screens Completed

--- HeerPiegonClub/src/AdminPanel/Pages/Touraments/Tournament.jsx
+++ HeerPiegonClub/src/AdminPanel/Pages/Touraments/Tournament.jsx
@@ -11,13 +11,19 @@ const Tournament = () => {
     { name: "Faisalabad" },
     { name: "Rawalpindi" },
     { name: "Multan" },
-    
   ];
-const [tournament,addTournament] = useState(false)
-const adding = ()=>
-{
-    addTournament(!tournament)
-}
+  const [tournament, addTournament] = useState(false);
+  const adding = () => {
+    addTournament(!tournament);
+  };
+  const [editTournament, setEditTournament] = useState(false);
+  const editing = () => {
+    setEditTournament(!editTournament);
+  };
+  const [addPerson, setAddPerson] = useState(false);
+  const addingPerson = () => {
+    setAddPerson(!addPerson);
+  };
   return (
     <div className={s.container}>
       <div className={s.top}>
@@ -25,9 +31,32 @@ const adding = ()=>
 
         <IoAddCircleOutline className={s.add} onClick={adding} />
       </div>
-      {tournament && (<div className={s.popup}>
-            <input />
-        </div>)}
+      {tournament && (
+        <div className={s.popup}>
+          <h1>New Tournament</h1>
+          <input type="text" />
+          <div className={s.dates}>
+            <div>
+              <h2>Start Date</h2>
+              <input type="date" />
+            </div>
+            <div>
+              <h2>End Date</h2>
+              <input type="date" />
+            </div>
+          </div>
+          <div className={s.button}>
+            <button className={s.cancelbtn} onClick={adding}>
+              {" "}
+              Cancel
+            </button>
+            <button className={s.addbtn} onClick={adding}>
+              {" "}
+              Add
+            </button>
+          </div>
+        </div>
+      )}
       <div className={s.table}>
         <table className={s.tournamenttable}>
           <thead>
@@ -45,11 +74,70 @@ const adding = ()=>
                 <td>{index + 1}</td>
                 <td>{tournament.name}</td>
                 <td>
-                  <AiOutlineUsergroupAdd className={s.addperson} />
+                  <AiOutlineUsergroupAdd
+                    className={s.addperson}
+                    onClick={addingPerson}
+                  />
+                  {addPerson && (
+                    <div className={s.popup}>
+                     <div className={s.personData}>
+                     <h1>Add Person</h1>
+                      
+                      <h3>Player Name</h3>
+                      <input type="text" />
+                    
+                      <h3>Player Address</h3>
+                      <input type="text" />
+                      <h3>Add Photo</h3>
+
+<input type="file" className={s.fileInput} />
+<button className={s.uploadButton} onClick={addingPerson}>
+  Upload Photo
+</button>
+                     </div>
+                      
+                      <div className={s.button}>
+                        <button className={s.cancelbtn} onClick={addingPerson}>
+                          {" "}
+                          Cancel
+                        </button>
+                        <button className={s.addbtn} onClick={addingPerson}>
+                          {" "}
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                  )}
                 </td>
                 <td>
-                  <BiEdit className={s.edit} />
+                  <BiEdit className={s.edit} onClick={editing} />
                 </td>
+                {editTournament && (
+                  <div className={s.popup}>
+                    <h1>Edit Tournament</h1>
+                    <input type="text" />
+                    <div className={s.dates}>
+                      <div>
+                        <h2>Start Date</h2>
+                        <input type="date" />
+                      </div>
+                      <div>
+                        <h2>End Date</h2>
+                        <input type="date" />
+                      </div>
+                    </div>
+                    <div className={s.button}>
+                      <button className={s.cancelbtn} onClick={editing}>
+                        {" "}
+                        Cancel
+                      </button>
+                      <button className={s.addbtn} onClick={editing}>
+                        {" "}
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                )}
                 <td>
                   <FaMinusCircle className={s.remove} />
                 </td>
